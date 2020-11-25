cb9fb37ece21db6e49db2cd4a32121c857b36b60
WindowsROOT <greanner@gmail.com>
2020-11-25 13:45:33 +0700

    WindowsROOT on CloudRun
    WindowsROOT on CloudRun

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "Google Cloud Run"
+		target = "WindowsROOT on CloudRun"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", target)
 }
